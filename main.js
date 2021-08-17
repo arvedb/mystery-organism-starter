0e7a03564d63c6264bf5f0a0993864f10aea1fe4
Arved B <acob91@gmx.de>
2021-08-17 17:18:51 +0200

    implemented pAequorFactory and mutate

--- main.js
+++ main.js
@@ -13,7 +13,50 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (specimenNum, dna) => {
+  return {
+    _specimenNum: specimenNum,
+    _dna: dna,
 
+    get specimenNum(){
+      return this._specimenNum;
+    },
+
+    get dna(){
+      return this._dna;
+    },
+
+    set specimenNum(value){
+      this._specimenNum = value;
+    },
+
+    set dna(value){
+      this._dna = value;
+    },
+
+    mutate(){
+      let randomDnaBaseIndex = Math.floor(Math.random()*4);
+      let originalBase = this._dna[randomDnaBaseIndex];
+
+      while(this._dna[randomDnaBaseIndex] === originalBase){
+        this._dna[randomDnaBaseIndex] = returnRandBase();
+      }
+
+      return this._dna;
+    }
+
+  }
+}
+
+
+
+
+
+// Test-Code
+
+let testSpecimen = pAequorFactory(2,mockUpStrand());
+console.log(testSpecimen);
+console.log(testSpecimen.mutate());
 
 
 
